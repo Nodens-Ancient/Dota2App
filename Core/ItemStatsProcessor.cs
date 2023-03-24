a109368169afd597e5aeb7af0fde29ef08355b11
Nodens-Ancient <60713065+Nodens-Ancient@users.noreply.github.com>
2023-03-24 14:18:08 +0300

    preview adding item stats

---
+++ Core/ItemStatsProcessor.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Dota2App.Models.DTO;
+
+namespace Dota2App.Core
+{
+    public class ItemStatsProcessor
+    {
+        public void AddBonusDamage(ref Hero hero, float bonusDamage)
+        {
+
+        }
+
+        public void AddBonusHealth(ref Hero hero, float health)
+        {
+
+        }
+
+        public void AddBonusHealthRegen(ref Hero hero, float healthRegen)
+        {
+
+        }
+
+        public void AddBonusMana(ref Hero hero, float mana)
+        {
+
+        }
+
+        public void AddBonusManaRegen(ref Hero hero, float manaRegen)
+        {
+
+        }
+
+        public void AddBonusArmor(ref Hero hero, float armor)
+        {
+            
+        }
+
+        public void AddBonusMagicResistance(ref Hero hero, float magicResistance)
+        {
+
+        }
+
+        public void AddBonusEvasion(ref Hero hero, float evasion)
+        {
+
+        }
+
+        public void AddBonusNightVision(ref Hero hero, float nightVision)
+        {
+
+        }
+
+
+    }
+}
